7800b375b2f0ccd80a64e846c23238911615c4b3
mdpos95-tech <mdpos95@gmail.com>
2026-05-31 22:04:11 +0100

    more python code and migrate added

--- app.py
+++ app.py
@@ -7,3 +7,22 @@ from models import db, Recipe, ContactMessage
 load_dotenv()
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "temporary_secret_key")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/recipes")
+def recipes():
+    all_recipes = Recipe.query.all()
+    return render_template("recipes.html", recipes=all_recipes) 
+
+if __name__ == "__main__":
+    app.run(debug=True)
