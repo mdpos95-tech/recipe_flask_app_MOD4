b84deb3fc482dc17895d8d61d414bc05bb26add0
mdpos95-tech <mdpos95@gmail.com>
2026-05-31 20:18:10 +0100

    initial models.py code

--- models.py
+++ models.py
@@ -5,8 +5,17 @@ db = SQLAlchemy()
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+title = db.Column(db.String(150), nullable=False)
+category = db.Column(db.String(50), nullable=False)
+ingredients = db.Column(db.Text, nullable=False)
+instructions = db.Column(db.Text, nullable=False)
+image_url = db.Column(db.String(255), nullable=True)
+created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 
-
-    class ContactMessage(db.Model):
+class ContactMessage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
